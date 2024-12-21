3c4bb93969871038e07ebe701415904ceab530b3
zorea <zoreasaf@gmail.com>
2024-12-21 21:57:03 +0200

    torchgate description

--- noisereduce/torchgate/__init__.py
+++ noisereduce/torchgate/__init__.py
@@ -1,8 +1,4 @@
 """
-TorchGating is a PyTorch-based implementation of Spectral Gating
-================================================
-Author: Asaf Zorea
-
 Contents
 --------
 torchgate imports all the functions from PyTorch, and in addition provides:
