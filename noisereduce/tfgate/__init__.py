f5597103bce09e701233ce465886d1634f5c3dcc
zorea <zoreasaf@gmail.com>
2024-12-21 21:56:44 +0200

    tfgate

---
+++ noisereduce/tfgate/__init__.py
@@ -0,0 +1,8 @@
+"""
+Contents
+--------
+torchgate imports all the functions from TensorFlow, and in addition provides:
+ TorchGating       --- A PyTorch module that applies a spectral gate to an input signal
+
+"""
+from .tfgate import TFGate
